e391c42c8eacba360e5c5c03499f4d51c82b7aaa
Joseph Vijay J <87023714+josephvijayJ@users.noreply.github.com>
2022-04-09 11:42:16 +0530

    Update Signup.js

--- src/components/Authentication/Signup.js
+++ src/components/Authentication/Signup.js
@@ -51,7 +51,7 @@ const Signup = () => {
         },
       };
       const { data } = await axios.post(
-        '/api/user',
+        'https://chat-app-backend1-2-3.herokuapp.com/api/user',
         {
           name,
           email,
