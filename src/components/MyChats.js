401fe2b35022b398e7dc1043dfa4863f5c07f52e
Joseph Vijay J <87023714+josephvijayJ@users.noreply.github.com>
2022-04-09 11:46:59 +0530

    Update MyChats.js

--- src/components/MyChats.js
+++ src/components/MyChats.js
@@ -25,7 +25,7 @@ const MyChats = ({ fetchAgain }) => {
         },
       };
 
-      const { data } = await axios.get("/api/chat", config);
+      const { data } = await axios.get("https://chat-app-backend1-2-3.herokuapp.com/api/chat", config);
       setChats(data);
     } catch (error) {
       toast({
